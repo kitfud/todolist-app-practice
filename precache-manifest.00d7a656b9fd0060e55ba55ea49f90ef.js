a8a43d1636c0bd87b0bad2bf47c1d383dbe44832
Kit Fuderich <kitfuderich@gmail.com>
2020-06-12 09:56:05 -0700

    Updates

---
+++ precache-manifest.00d7a656b9fd0060e55ba55ea49f90ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0dda897810bd8a7c50ee15ef2743ef5",
+    "url": "/todolist-app-practice/index.html"
+  },
+  {
+    "revision": "cd090bbd5bd7804da9dd",
+    "url": "/todolist-app-practice/static/js/2.4101e239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-app-practice/static/js/2.4101e239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ef506b19a1978d89ac",
+    "url": "/todolist-app-practice/static/js/main.04381191.chunk.js"
+  },
+  {
+    "revision": "5f46294ad32e5a0a9f7e",
+    "url": "/todolist-app-practice/static/js/runtime-main.1f19d02e.js"
+  }
+]);
