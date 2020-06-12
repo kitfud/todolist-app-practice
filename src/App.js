dc14e506e72d3c1eedfca61dcafeda5859fb198f
Kit Fuderich <kitfuderich@gmail.com>
2020-06-12 09:19:45 -0700

    saving across page rreloads with useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList'
 import { v4 as uuidv4 } from 'uuid';
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
- const [todos,setTodos]= useState([
- 
-  ])//first state is all variables and second is funciton passed through all
+ const [todos,setTodos]= useState([])//first state is all variables and second is funciton passed through all
 
+useEffect(()=>{//reloads across page persists
+  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  if(storedTodos) setTodos(storedTodos)
+},[]) 
+
+
+useEffect (()=>{//stores each item within a page load
+localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
+},[todos])
+  
 const todoNameRef = useRef()
 
  function handleAddTodo(e){
@@ -22,9 +31,7 @@ const todoNameRef = useRef()
    <React.Fragment>
    <TodoList todos={todos}/>
    <input ref = {todoNameRef} type="text"/>
-   <button 
-   onClick= {handleAddTodo}
-   >Add Todo</button>
+   <button onClick= {handleAddTodo}>Add Todo</button>
    <button>Clear Complete</button>
    </React.Fragment>
 
