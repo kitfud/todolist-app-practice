3ab58d05af90b515ef43524ee27dfbfb96b0dcdf
Kit Fuderich <kitfuderich@gmail.com>
2020-06-12 08:11:54 -0700

    simple props passed example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from './TodoList'
 
 
 function App() {
+ const [todos,setTodos]= useState(['task1','task2'])//first state is all variables and second is funciton passed through all
+
  return (
    <React.Fragment>
-   <TodoList />
+   <TodoList todos={todos}/>
    <input type="text"/>
    <button>Add Todo</button>
    <button>Clear Complete</button>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function TodoList() {
+export default function TodoList({todos}) {
     return (
         <div>
-            Hello world!
+           {todos.length}
         </div>
     )
 }
